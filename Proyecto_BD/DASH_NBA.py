608191a8001b7be16890ea2ae612f0065fa8dbcc
Angelopezmacc <49574428+Angelopezmacc@users.noreply.github.com>
2021-05-08 18:54:13 -0500

    Update DASH_NBA.py

--- Proyecto_BD/DASH_NBA.py
+++ Proyecto_BD/DASH_NBA.py
@@ -1,7 +1,19 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri May  7 23:36:48 2021
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as px
+import pandas as pd
+from Connection import Connection
+import consultas as sql
 
-@author: gianc
-"""
+external_stylesheets = ["https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css"]
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+con = Connection()
+con.openConnection()
+
+
+
+con.closeConnection()
 
